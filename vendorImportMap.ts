05e7d8ea58319407a66f376e1b3de906f6e9be50
Benten <ben@benten.ca>
2023-07-13 22:20:49 -0600

    progress

--- vendorImportMap.ts
+++ vendorImportMap.ts
@@ -1,4 +1,5 @@
 import parseImports, { Import } from "npm:parse-imports";
+import slash from "https://deno.land/x/slash/mod.ts";
 import {
   basename,
   dirname,
@@ -26,21 +27,14 @@ const headers = new Headers({
     "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",
 });
 
-function isExternalUrl(url: string) {
-  return url.startsWith("http://") || url.startsWith("https://");
-}
-
 export default async function vendorImportMap(importMap: ImportMap) {
   if (!importMap.imports) return;
   const newImportMap = importMap;
   for await (const [module, specifier] of Object.entries(importMap.imports)) {
     if (!specifier) continue;
     if (isExternalUrl(specifier)) {
-      const specifierURL = new URL(specifier);
-      const specifierPath = `./vendor/${specifierURL.host}${specifierURL.pathname}`
-      console.log(specifierPath)
-      await vendorModule(specifierPath, specifier);
-      newImportMap.imports![module] = specifierPath;
+      const localPath = await vendorModule(specifier);
+      newImportMap.imports![module] = localPath;
     }
   }
   console.log(newImportMap);
@@ -50,53 +44,70 @@ export default async function vendorImportMap(importMap: ImportMap) {
 const importMap = {
   imports: {
     "react": "https://esm.sh/react",
+    "react-dom": "https://esm.sh/react-dom",
   },
 };
 
-await vendorImportMap(importMap);
+vendorImportMap(importMap);
 
-
-function combinePaths(path1: string, path2: string) {
-  return join(dirname(path1), dirname(path2)) + "/" + basename(path2)
+function isExternalUrl(url: string) {
+  return url.startsWith("http://") || url.startsWith("https://");
 }
 
-function externalPathToInternalPath(path: string) {
-  const specifierURL = new URL(path);
-  return `./vendor/${specifierURL.host}${specifierURL.pathname}`
-}
+async function vendorModule(path: string) {
+  /*
+    We need to create a local path from this url. This will be the location this module will be stored.
+    We parse imports. External URL's are fine, we don't need to do anything with them.
+    If the path is relative, eg. ./ or ../ we need to resolve it to an absolute path with current module external url.
+    If the path is absolute, we need to resolve it to an absolute path with the root url.
+  */
 
-// await vendorImportMap(importMap);
+  const module = await fetch(path, { headers });
+  if (!module.ok) throw new Error(`Unable to fetch module ${path}`);
+  let moduleText = await module.text();
 
-// imported module should know it's own path
-// it fetches itself, and then parses the imports
-// it turns the import paths into local vendored paths,
-// and then changes the import statements to use those paths
-// it then writes itself to the vendored path
-// finally, it calls itself recursively on the import statements
+  for await (const module of await parseImports(moduleText)) {
+    const modulePath = module.moduleSpecifier.value!;
 
-async function vendorModule(targetPath: string, moduleURL: string) {
-  const fetchedModule = await fetch(moduleURL, { headers });
-  let moduleContent = await fetchedModule.text();
-  const imports = await parseImports(moduleContent);
-  if(!imports) return;
-  for await (const importStatement of imports) {
-  
-    if(!importStatement.moduleSpecifier.value) continue;
+    if (!modulePath) continue;
 
-    let importTargetPath: string;
-    let importModuleURL: string;
+    if (isExternalUrl(modulePath)) {
+      // if path is external
+      const currentModuleVendorPath = externalPathToVendorPath(path);
+      const externalModuleVendorPath = externalPathToVendorPath(modulePath);
 
-    if(isExternalUrl(importStatement.moduleSpecifier.value)){
-      importTargetPath = externalPathToInternalPath(importStatement.moduleSpecifier.value);
-      importModuleURL = importStatement.moduleSpecifier.value;
-    } else {
-      importTargetPath = combinePaths(targetPath, importStatement.moduleSpecifier.value);
-      importModuleURL = new URL(importStatement.moduleSpecifier.value, moduleURL).toString();
+      const relativePath = slash(
+        relative(currentModuleVendorPath, externalModuleVendorPath).replace(
+          ".",
+          "",
+        ),
+      );
+
+      moduleText = moduleText.replaceAll(modulePath, relativePath);
+
+      await vendorModule(modulePath);
+
+    } else if (modulePath.startsWith("/")) {
+      // if path is absolute
+      const moduleUrl = new URL(path).origin + modulePath
+      console.log(externalPathToVendorPath(moduleUrl))
+      // need to go up to the vendor path, then back down to the new module path
+      moduleText = moduleText.replaceAll(modulePath, externalPathToVendorPath(moduleUrl));
+      await vendorModule(moduleUrl);
     }
-
-    await vendorModule(importTargetPath, importModuleURL);
-    moduleContent = moduleContent.replace(importStatement.moduleSpecifier.value, importTargetPath);
-    await Deno.mkdir(dirname(importTargetPath), { recursive: true });
-    await Deno.writeTextFile(importTargetPath, moduleContent);
   }
+  const folderPath = dirname(externalPathToVendorPath(path));
+  await Deno.mkdir(folderPath, { recursive: true });
+  const fileName = extname(basename(path)) ? basename(path) : "index.js";
+  await Deno.writeTextFile(folderPath + "/" + fileName, moduleText);
+  return fileName;
+}
+
+function externalPathToVendorPath(path: string) {
+  const url = new URL(path);
+  return `./vendor/${url.host}${url.pathname}`;
+}
+
+function ensureFileExtension(path: string) {
+  return extname(path) ? path : path + ".js";
 }
